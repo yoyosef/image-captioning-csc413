e935bb94619c6102b53db244894ae78bd094e4ab
yoyosef <yosefleibman@gmail.com>
2021-04-05 21:34:01 -0400

    encoder decoder

---
+++ encoder_decoder.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+# ref: https://github.com/yunjey/pytorch-tutorial/tree/master/tutorials/03-advanced/image_captioning 
+from torch.nn.utils.rnn import pack_padded_sequence
+
+class ResNetEncoder(nn.Module):
+    def __init__(self, embed_size, bn_momentum=0.01):
+        super(ResNetEncoder, self).__init__()
+        resnet = models.resnet34(pretrained=True)
+        modules = list(resnet.children())[:-1]
+        self.resnet = nn.Sequential(*modules)
+        self.linear = nn.Linear(resnet.fc.in_features, embed_size)
+        self.batchnorm = nn.BatchNorm1d(embed_size, momentum=bn_momentum)
+
+    def init_weights(self):
+        """Initialize the weights."""
+        self.linear.weight.data.normal_(0.0, 0.02)
+        self.linear.bias.data.fill_(0)
+
+    def forward(self, images):
+        with torch.no_grad():
+            features = self.resnet(images)
+        features = features.reshape(features.size(0), -1)
+        features = self.batchnorm(self.linear(features))
+        return features
+
+class Decoder(nn.Module):
+    def __init__(self, vocab_size, embedding_size, hidden_size, num_layers=1):
+        super(Decoder, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_size)
+        self.lstm = nn.LSTM(embedding_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+        self.init_weights()
+    
+    def init_weights(self):
+        """Initialize weights."""
+        self.embedding.weight.data.uniform_(-0.1, 0.1)
+        self.linear.weight.data.uniform_(-0.1, 0.1)
+        self.linear.bias.data.fill_(0)
+
+    def forward(self, features, captions, lengths):
+        """ This uses teacher forcing """
+        embeddings = self.embedding(captions)
+        print(embeddings.shape)
+        embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)
+        print(embeddings.shape)
+        # packed = pack_padded_sequence(embeddings, lengths, batch_first=True, enforce_sorted=False) 
+        
+        hiddens, _ = self.lstm(embeddings)
+        
+        outputs = self.linear(hiddens)
+        return outputs
