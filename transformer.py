4276e18ebaff7a9b67ca4aba95defa5fc2771444
Jeffrey Wong <jef.wong@mail.utoronto.ca>
2021-04-10 15:11:08 -0400

    Edited transformer.py

--- transformer.py
+++ transformer.py
@@ -34,13 +34,13 @@ class ScaledDotAttention(nn.Module):
         # ------------
         # FILL THIS IN
         # ------------
-        batch_size = queries.shape[0]
-        q = self.Q(queries.view(batch_size, -1, queries.shape[-1]))
+        batch_size = keys.size(0)
+        q = self.Q(queries)
         k = self.K(keys)
         v = self.V(values)
-        unnormalized_attention = k@q.transpose(2,1)*self.scaling_factor
+        unnormalized_attention = torch.bmm(q, k.transpose(1,2)) * self.scaling_factor 
         attention_weights = self.softmax(unnormalized_attention)
-        context = attention_weights.transpose(2,1)@v
+        context = torch.bmm(attention_weights, v)
         return context, attention_weights
 
 
@@ -75,93 +75,15 @@ class CausalScaledDotAttention(nn.Module):
         # ------------
         # FILL THIS IN
         # ------------
-        batch_size = queries.shape[0]
-        q = self.Q(queries.view(batch_size, -1, queries.shape[-1]))
+        batch_size = keys.size(0)
+        q = self.Q(queries)
         k = self.K(keys)
         v = self.V(values)
-        unnormalized_attention = k @ q.transpose(2, 1)*self.scaling_factor
-        mask = ~torch.triu(unnormalized_attention).bool()
-        attention_weights = self.softmax(
-            unnormalized_attention.masked_fill(mask, self.neg_inf))
-        context = attention_weights.transpose(2, 1) @ v
+        unnormalized_attention = torch.bmm(q, k.transpose(1,2)) * self.scaling_factor 
+        mask = self.neg_inf * torch.triu(torch.ones_like(unnormalized_attention))
+        attention_weights = self.softmax(unnormalized_attention + mask) 
+        context = torch.bmm(attention_weights, v) 
         return context, attention_weights
-
-
-
-class TransformerEncoder(nn.Module):
-    def __init__(self, vocab_size, hidden_size, num_layers, opts):
-        super(TransformerEncoder, self).__init__()
-
-        self.vocab_size = vocab_size
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.opts = opts
-
-        self.embedding = nn.Embedding(vocab_size, hidden_size)
-
-        self.self_attentions = nn.ModuleList([ScaledDotAttention(
-            hidden_size=hidden_size,
-        ) for i in range(self.num_layers)])
-        self.attention_mlps = nn.ModuleList([nn.Sequential(
-            nn.Linear(hidden_size, hidden_size),
-            nn.ReLU(),
-        ) for i in range(self.num_layers)])
-
-        self.positional_encodings = self.create_positional_encodings()
-
-    def forward(self, inputs):
-        """Forward pass of the encoder RNN.
-
-        Arguments:
-            inputs: Input token indexes across a batch for all time steps in the sequence. (batch_size x seq_len)
-
-        Returns:
-            annotations: The hidden states computed at each step of the input sequence. (batch_size x seq_len x hidden_size)
-            None: Used to conform to standard encoder return signature.
-            None: Used to conform to standard encoder return signature.        
-        """
-        batch_size, seq_len = inputs.size()
-
-        encoded = self.embedding(inputs)  # batch_size x seq_len x hidden_size
-
-        # Add positinal embeddings from self.create_positional_encodings. (a'la https://arxiv.org/pdf/1706.03762.pdf, section 3.5)
-        encoded = encoded + self.positional_encodings[:seq_len]
-
-        annotations = encoded
-        for i in range(self.num_layers):
-            new_annotations, self_attention_weights = self.self_attentions[i](
-                annotations, annotations, annotations)  # batch_size x seq_len x hidden_size
-            residual_annotations = annotations + new_annotations
-            new_annotations = self.attention_mlps[i](residual_annotations)
-            annotations = residual_annotations + new_annotations
-
-        # Transformer encoder does not have a last hidden or cell layer.
-        return annotations, None, None
-
-    def create_positional_encodings(self, max_seq_len=1000):
-        """Creates positional encodings for the inputs.
-
-        Arguments:
-            max_seq_len: a number larger than the maximum string length we expect to encounter during training
-
-        Returns:
-            pos_encodings: (max_seq_len, hidden_dim) Positional encodings for a sequence with length max_seq_len. 
-        """
-        pos_indices = torch.arange(max_seq_len)[..., None]
-        dim_indices = torch.arange(self.hidden_size//2)[None, ...]
-        exponents = (2*dim_indices).float()/(self.hidden_size)
-        trig_args = pos_indices / (10000**exponents)
-        sin_terms = torch.sin(trig_args)
-        cos_terms = torch.cos(trig_args)
-
-        pos_encodings = torch.zeros((max_seq_len, self.hidden_size))
-        pos_encodings[:, 0::2] = sin_terms
-        pos_encodings[:, 1::2] = cos_terms
-
-        if self.opts.cuda:
-            pos_encodings = pos_encodings.cuda()
-
-        return pos_encodings
 
 
 class TransformerDecoder(nn.Module):
